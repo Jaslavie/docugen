285316c7933e1b7d824ecbdda013b5a983cb303b
Jasmine <jasmineqw45@gmail.com>
2024-11-24 10:38:39 -0800

    successfully parsed pdf files

--- backend/src/scripts/pdf_convert.py
+++ backend/src/scripts/pdf_convert.py
@@ -1,10 +1,10 @@
-import asyncio
-from pdfminer.high_level import extract_text_to_fp
+
+from pdfminer.high_level import extract_text 
 
 # extract the binary representation of the pdf file
-async def pdf_to_text(pdf_buffer: bytes) -> str:
+def pdf_to_text(pdf_buffer: bytes) -> str:
     text = ''
-    with open('temp.pdf', 'wb') as temp_pdf:
+    with open('temp.pdf', 'wb') as temp_pdf: # write the pdf buffer to a temporary file
         temp_pdf.write(pdf_buffer)
-    text = extract_text_to_fp('temp.pdf')
+    text = extract_text('temp.pdf')
     return text
